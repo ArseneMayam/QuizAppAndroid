f8b85bf8889645c234eb221c54af79fe56c135ff
Arsene Mayam <mayamarsene@yahoo.com>
2017-11-14 14:29:50 -0500

    Commit quizActivity

--- app/src/main/java/com/example/arsene/quizappandroid/QuizActivity.java
+++ app/src/main/java/com/example/arsene/quizappandroid/QuizActivity.java
@@ -3,6 +3,7 @@ package com.example.arsene.quizappandroid;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -102,6 +103,37 @@ public class QuizActivity extends AppCompatActivity {
 
         // efface textView resultat
         resultatTxtView.setText(" ");
+
+        // efface les buttons de choix précédents
+        for (int row = 0; row < buttonTableLayout.getChildCount(); row++){
+            ((TableRow) buttonTableLayout.getChildAt(row)).removeAllViews();
+        }
+
+        // ajoute 3, 6 ou 9 buttons en fonction de nombreChoixAff
+        for (int row = 0; row < nombreChoixAff; row++){
+            TableRow ligneBttnCourrant = getTableRow(row);
+
+            // inflate le button choix
+            for (int column = 0; column<3; column++){
+                //inflate button
+                Button newChoixBttn = (Button) getLayoutInflater().inflate(R.layout.choix_bttn,null);
+
+                // get liste des choix et set text des buttons
+
+                // ecoute sur les buttons
+                newChoixBttn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        submitButton((Button) view);
+                    }
+                });
+                // ajoute button au tableRow courrant
+                ligneBttnCourrant.addView(newChoixBttn);
+            }
+        }
+
+        // on remplace le text d'un boutton par la reponse correct
+        // set sont id = id_question
 
 
     }
